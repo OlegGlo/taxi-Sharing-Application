30a84863a4db4d9e40707a96de2594b01e66d1bb
magarelt <magarelt@mcmaster.ca>
2023-04-07 23:51:14 -0400

    java classes for rating, request, incident, edit req fragments

---
+++ app/src/main/java/com/example/fareshare/RatingFragment.java
@@ -0,0 +1,52 @@
+package com.example.fareshare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.fareshare.databinding.FragmentRatingBinding;
+
+public class RatingFragment extends Fragment {
+
+    private FragmentRatingBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentRatingBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+    @Override
+    protected void onViewCreate(@NonNull View view, Bundle savedInstanceState) {
+        super.onCreate(view, savedInstanceState);
+
+        RatingBar ratingBar = (RatingBar) view.findViewById(R.id.ratingBar);
+        Button submitButton = (Button) view.findViewById(R.id.submitRatingButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(RatingFragment.this)
+                        .navigate(R.id.action_ratingFragment_to_homeFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
